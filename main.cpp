357b606a211a45e2281a977cea05fc13cf229f8a
Stephen Inglis <dartonias@gmail.com>
2014-07-02 13:39:36 +0200

    Some changes to parameters, will introduce better system for temperature

--- main.cpp
+++ main.cpp
@@ -3,15 +3,45 @@
 #include <cmath>
 
 int main(int argc, char** argv){
-    Sim* mySim = new Sim();
-    for(int i_bins=0;i_bins<mySim->bins;i_bins++){
-        for(int i_sweeps=0;i_sweeps<mySim->sweeps;i_sweeps++){
-            for(int i_onesweep=0;i_onesweep<mySim->getNspins();i_onesweep++){
-                mySim->singleUpdate();
+    Eigen::Matrix<int, Eigen::Dynamic, 1> temp_spins;
+    double beta_min;
+    double beta_max;
+    int num_temps;
+    runParams(beta_min,beta_max,num_temps);
+    vector<Sim> simVec;
+    simVec.resize(0);
+    simVec.push_back(Sim());
+    if(fexists("LOADJ")){
+        simVec[0].loadJ();
+    }
+    if(fexists("SAVEJ")){
+        simVec[0].saveJ();
+        return 0;
+    }
+    for(int i=0;i<(num_temps-1);i++){
+        simVec.push_back(Sim(simVec[0].getRand(),simVec[0].getJ(),(beta_max-beta_min)/(num_temps-1)*(i+1)+beta_min));
+    }
+    for(int i_bins=0;i_bins<simVec[0].bins;i_bins++){
+        for(int i_sweeps=0;i_sweeps<simVec[0].sweeps;i_sweeps++){
+            for(int z=0;z<num_temps;z++){
+                for(int i_onesweep=0;i_onesweep<simVec[0].getNspins();i_onesweep++){
+                    simVec[z].singleUpdate();
+                }
+                simVec[z].clusterUpdate();
+                simVec[z].updateBinder();
+                simVec[z].updateE();
             }
-            mySim->clusterUpdate();
-            //mySim->updateE();
-            mySim->updateBinder();
+            // Parallel tempering step
+            for(int z=0;z<num_temps;z++){
+                int flipme = simVec[0].getRand()->randInt(num_temps-2);
+                double dE = simVec[flipme+1].getE() - simVec[flipme].getE();
+                double dB = simVec[flipme+1].getB() - simVec[flipme].getB();
+                if(simVec[0].getRand()->randExc() < exp(dE*dB)){
+                    temp_spins = simVec[flipme].getSpins();
+                    simVec[flipme].setSpins(simVec[flipme+1].getSpins());
+                    simVec[flipme+1].setSpins(temp_spins);
+                }
+            }
         }
     }
     return 0;
--- sim.hpp
+++ sim.hpp
@@ -22,9 +22,9 @@ class Sim{
     private:
         static const int binSize = 1000; // Averages per line (larger --> smaller files)
         static const int bufferSize = 10; // Lines held per write (larger --> less often writes to file)
-        Eigen::Matrix<int, Eigen::Dynamic, 1> spins; // All spins +- 1
         Eigen::Matrix<int, Eigen::Dynamic, 1> cluster; // For the cluster update
         Eigen::Matrix<int, Eigen::Dynamic, 1> Jmat; // All elements +- 1
+        Eigen::Matrix<int, Eigen::Dynamic, 1> spins; // All spins +- 1
         void loadParams(); // Loads parameters from param.dat
         MTRand* rand; // MersenneTwister pseudorandom number generator
         int seed; // Seed for the random number generator
@@ -36,24 +36,30 @@ class Sim{
         int regionA; // Number of rows in regionA (for simplicity, we will only add one entire row at a time)
         double Padd; // Probability of adding to a cluster
         double Energy; // Total energy
-        observable<double>* obs_energy;
         observable<double>* obs_mag2; // For binder cumulant
         observable<double>* obs_mag4;
     public:
         Sim(); //Default constructor
+        Sim(MTRand* _rand, Eigen::Matrix<int, Eigen::Dynamic, 1> _Jmat, double _beta); //Parallel temperting constructor
+        Eigen::Matrix<int, Eigen::Dynamic, 1> getSpins();
+        void setSpins(Eigen::Matrix<int, Eigen::Dynamic, 1> _spins);
         int sweeps; // Number of MC sweeps per bin
         int bins; // Number of total bins
         void singleUpdate(); // Single spin update attempt
         int adjS(int x, int y); // Find the y'th neighbor to spin x
         int adjJ(int x, int y); // Find the y'th bond of spin x (it connect's with the y'th spin above)
-        // Cluster update
         void addNeighbours(int z, int& s);
         void clusterUpdate(); // Swendsen-Wang update
         int getNspins();
         int getNbonds();
         void updateE(); // Updates the Energy variable above
         void updateBinder(); // Updates magnetization for Binder Ratio
-
+        void saveJ();
+        void loadJ();
+        Eigen::Matrix<int, Eigen::Dynamic, 1> getJ();
+        MTRand* getRand();
+        double getE();
+        double getB();
 };
 
 int Sim::getNspins(){
@@ -66,9 +72,8 @@ int Sim::getNbonds(){
 
 Sim::Sim(){
     loadParams();
-    obs_energy = new observable<double>("energy",bufferSize,binSize,0);
-    obs_mag2 = new observable<double>("mag2",bufferSize,binSize,0);
-    obs_mag4 = new observable<double>("mag4",bufferSize,binSize,0);
+    obs_mag2 = new observable<double>("mag2_"+std::to_string(beta),bufferSize,binSize,0);
+    obs_mag4 = new observable<double>("mag4_"+std::to_string(beta),bufferSize,binSize,0);
     Padd = 1. - exp(-2*beta); // Probability of adding a spin when they are satisfied for the cluster move
     rand = new MTRand(seed);
     Nspins = L*L;
@@ -92,13 +97,36 @@ Sim::Sim(){
     }
 }
 
+Sim::Sim(MTRand* _rand, Eigen::Matrix<int, Eigen::Dynamic, 1> _Jmat, double _beta){
+    loadParams();
+    beta = _beta;
+    obs_mag2 = new observable<double>("mag2_"+std::to_string(beta),bufferSize,binSize,0);
+    obs_mag4 = new observable<double>("mag4_"+std::to_string(beta),bufferSize,binSize,0);
+    Padd = 1. - exp(-2*beta); // Probability of adding a spin when they are satisfied for the cluster move
+    rand = _rand;
+    Nspins = L*L;
+    Nbonds = 2*Nspins;
+    spins.resize(Nspins);
+    cluster.resize(Nspins);
+    for(int i=0;i<Nspins;i++){
+        spins(i) = rand->randInt(1)*2 - 1; // +- 1 random initial state
+    }
+    Jmat.resize(Nbonds);
+    Jmat = _Jmat;
+    if(DEBUG){
+        std::cout << Jmat.transpose() << std::endl;
+    }
+}
+
 void Sim::loadParams(){
     std::string filename = "param.dat";
     std::string g; // Garbage string for going through param file
     std::fstream inFile(filename.c_str());
     inFile >> g >> L;
     inFile >> g >> P;
-    inFile >> g >> beta;
+    inFile >> g >> beta; //beta_min
+    inFile >> g >> g; //beta_max
+    inFile >> g >> g; //num_temps
     inFile >> g >> seed;
     inFile >> g >> sweeps;
     inFile >> g >> bins;
@@ -238,13 +266,72 @@ void Sim::updateE(){
             Energy += spins(i) * spins(adjS(i,b)) * Jmat(adjJ(i,b));
         }
     }
-    obs_energy->pe(Energy/Nspins);
 }
 
 void Sim::updateBinder(){
     double mag = spins.sum()*1.0/Nspins;
     obs_mag2->pe(pow(mag,2));
     obs_mag4->pe(pow(mag,4));
+}
+
+void Sim::saveJ(){
+    ofstream jfile;
+    jfile.open("Jmat.dat");
+    for(int i=0;i<Nbonds;i++){
+        jfile << Jmat(i);
+    }
+    jfile.close();
+}
+
+void Sim::loadJ(){
+    ifstream jfile;
+    jfile.open("Jmat.dat");
+    for(int i=0;i<Nbonds;i++){
+        jfile >> Jmat(i);
+    }
+    jfile.close();
+}
+
+Eigen::Matrix<int, Eigen::Dynamic, 1> Sim::getJ(){
+    return Jmat;
+}
+
+MTRand* Sim::getRand(){
+    return rand;
+}
+
+double Sim::getE(){
+    return Energy;
+}
+
+double Sim::getB(){
+    return beta;
+}
+
+Eigen::Matrix<int, Eigen::Dynamic, 1> Sim::getSpins(){
+    return spins;
+}
+
+void Sim::setSpins(Eigen::Matrix<int, Eigen::Dynamic, 1> _spins){
+    spins = _spins;
+}
+
+// Some helper functions, for flow control
+
+bool fexists(const char* filename){
+    ifstream ifile(filename);
+    return ifile;
+}
+
+void runParams(double& beta_min, double& beta_max, int& num_temps){
+    std::string filename = "param.dat";
+    std::string g; // Garbage string for going through param file
+    std::fstream inFile(filename.c_str());
+    inFile >> g >> g;
+    inFile >> g >> g;
+    inFile >> g >> beta_min; //beta_min
+    inFile >> g >> beta_max; //beta_max
+    inFile >> g >> num_temps; //num_temps
 }
 
 #endif //SIMHPP
