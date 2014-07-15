7f7829dcda607f78a19d1222bf522d9bcb5e6a2f
Stephen Inglis <dartonias@gmail.com>
2014-07-15 14:01:15 +0200

    Small changes to temperature schedule

--- Temps.py
+++ Temps.py
@@ -1,14 +1,9 @@
-bmin = 0.4
-bmax = 0.6
-N = 20
+bmin = 0.5
+bmax = 1.5
+N = 40
 b = [(bmax-bmin)*i/N + bmin for i in range(N+1)]
 
-bmin = 0.430
-bmax = 0.450
-N = 20
-b += [(bmax-bmin)*i/N + bmin for i in range(N+1)]
-
-b += [0.1,0.2,0.3]
+b += [0.1,0.2,0.3,0.4]
 
 b = list(set(b))
 b = sorted(b,key=lambda x:x)
